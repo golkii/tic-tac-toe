75c47d9d87540502968915ea2175f3c8037a6da7
golki <antgolik@gmail.com>
2023-01-26 14:05:31 +0300

    Created start display

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ const Gameboard = (function () {
     const getSign = () => sign;
     const getTurn = () => turn;
     const updateTurn = () => turn = !turn;
+    const setName = (name1) => name = name1;
     const makeAMove = (i, j, sign) => {
       gameboard[i][j] = sign;
     }
 
-    return { getName, getSign, getTurn, makeAMove, updateTurn };
+    return { getName, getSign, getTurn, makeAMove, updateTurn, setName };
   };
 
   let player1 = Player('x', 'player 1', true);
@@ -40,20 +41,16 @@ const Gameboard = (function () {
 
   const checkWin = () => {
     if (gameboard[0][0] == gameboard[1][1] && gameboard[0][0] == gameboard[2][2] && gameboard[0][0] != '') {
-      console.log('диагональ');
       return true;
     }
     else if (gameboard[2][0] == gameboard[1][1] && gameboard[2][0] == gameboard[0][2] && gameboard[2][0] != '') {
-      console.log('обратная диагональ');
       return true;
     }
     for (let i = 0; i < 3; i++) {
       if (gameboard[i][0] == gameboard[i][1] && gameboard[i][0] == gameboard[i][2] && gameboard[i][0] != '') {
-        console.log('горизонтальный ряд');
         return true;
       }
       else if (gameboard[0][i] == gameboard[1][i] && gameboard[0][i] == gameboard[2][i] && gameboard[0][i] != '') {
-        console.log('Вертикальный ряд');
         return true;
       }
     }
@@ -67,8 +64,57 @@ const Gameboard = (function () {
 
 const displayController = (function () {
 
+  const createBeginDisplay = () => {
+    let div = document.createElement('div');
+    div.classList.add('begin-display');
+    let withFiend = document.createElement('button');
+    withFiend.textContent = 'Play with fiend';
+    let withAI = document.createElement('button');
+    withAI.textContent = 'Play with AI';
+    withFiend.classList.add('buttons');
+    withAI.classList.add('buttons');
+
+    withFiend.addEventListener('click', (e) => {
+      let head = document.querySelector('h3');
+      head.textContent = 'Type names';
+      let firstName = document.createElement('input');
+      firstName.type = 'text';
+      firstName.value = 'Player 1';
+      div.append(firstName);
+      let secondName = document.createElement('input');
+      secondName.type = 'text';
+      secondName.value = 'Player 2';
+      div.append(secondName);
+
+      withFiend.remove();
+      withAI.remove();
+
+      let startButton = document.createElement('button');
+      startButton.classList.add('buttons');
+      startButton.textContent = 'Start';
+      document.body.append(startButton);
+      startButton.addEventListener('click', (e) => {
+        Gameboard.player1.setName(firstName.value);
+        Gameboard.player2.setName(secondName.value);
+
+        firstName.remove();
+        secondName.remove();
+        startButton.remove();
+
+        head.textContent = Gameboard.player1.getName() + "'s move";
+
+        renderGameboard();
+      });
+    });
+
+    withAI.addEventListener('click', (e) => {
+
+    });
+    div.append(withFiend, withAI);
+    document.body.append(div);
+  }
+
   const renderGameboard = () => {
-    console.log('начал делать поле');
     let div = document.createElement('div');
     div.classList.add('game-board');
     for (let i = 0; i < 3; i++) {
@@ -84,8 +130,7 @@ const displayController = (function () {
             Gameboard.player1.updateTurn();
             Gameboard.player2.updateTurn();
             let header = document.querySelector('h3');
-            header.textContent = 'Player 2 move';
-            console.log('ход 1 игрока');
+            header.textContent = Gameboard.player2.getName() + "'s move";
           }
           else if (Gameboard.player2.getTurn() && !Gameboard.checkWin()) {
             Gameboard.player2.makeAMove(e.target.id[0], e.target.id[2], Gameboard.player2.getSign());
@@ -93,19 +138,17 @@ const displayController = (function () {
             Gameboard.player1.updateTurn();
             Gameboard.player2.updateTurn();
             let header = document.querySelector('h3');
-            header.textContent = 'Player 1 move';
-            console.log('Ход 2 игрока');
+            header.textContent = Gameboard.player1.getName() + "'s move";
           }
           if (Gameboard.checkWin()) {
             let header = document.querySelector('h3');
             if (Gameboard.player1.getTurn()) {
-              header.textContent = 'Player 2 WIN!';
+              header.textContent = Gameboard.player2.getName() + ' WIN!';
             }
             else {
-              header.textContent = 'Player 1 WIN!';
+              header.textContent = Gameboard.player1.getName() + ' WIN!';
             }
             reset.classList.remove('hide');
-            console.log('конец игры');
           }
         });
         div.append(gameDot);
@@ -126,37 +169,30 @@ const displayController = (function () {
     div.remove();
     Gameboard.resetGameboard();
     Gameboard.resetPlayers();
-    console.log(Gameboard.player1.getTurn());
-    console.log(Gameboard.player2.getTurn());
     reset.classList.add('hide');
     Gameboard.checkWin = () => {
       if (Gameboard.gameboard[0][0] == Gameboard.gameboard[1][1] && Gameboard.gameboard[0][0] == Gameboard.gameboard[2][2] && Gameboard.gameboard[0][0] != '') {
-        console.log('диагональ');
         return true;
       }
       else if (Gameboard.gameboard[2][0] == Gameboard.gameboard[1][1] && Gameboard.gameboard[2][0] == Gameboard.gameboard[0][2] && Gameboard.gameboard[2][0] != '') {
-        console.log('обратная диагональ');
         return true;
       }
       for (let i = 0; i < 3; i++) {
         if (Gameboard.gameboard[i][0] == Gameboard.gameboard[i][1] && Gameboard.gameboard[i][0] == Gameboard.gameboard[i][2] && Gameboard.gameboard[i][0] != '') {
-          console.log('горизонтальный ряд');
           return true;
         }
         else if (Gameboard.gameboard[0][i] == Gameboard.gameboard[1][i] && Gameboard.gameboard[0][i] == Gameboard.gameboard[2][i] && Gameboard.gameboard[0][i] != '') {
-          console.log('Вертикальный ряд');
           return true;
         }
       }
       return false;
     };
     renderGameboard();
-    console.log('Начал заново');
     let header = document.querySelector('h3');
-    header.textContent = 'Player 1 move';
+    header.textContent = Gameboard.player1.getName() + "'s move";
   });
 
-  return { renderGameboard, updateGameBoard };
+  return { createBeginDisplay, renderGameboard, updateGameBoard };
 })();
 
-displayController.renderGameboard();
+displayController.createBeginDisplay();
